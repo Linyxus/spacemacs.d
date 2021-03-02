759a07dcc9b10c00fcf550826730ed5daf01bc1d
Yichen Xu <yichen.x@outlook.com>
2021-03-02 18:15:45 +0800

    Update init.el

--- init.el
+++ init.el
@@ -32,7 +32,8 @@ This function should only modify configuration layer settings."
 
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
-   '(shell-scripts
+   '(
+     shell-scripts
      (lsp :variables
           default-nix-wrapper (lambda (args)
                                 (append
