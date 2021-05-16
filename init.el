7fe8391113466fbd476f6e3f4d1a9622438ae6a6
Yichen Xu <yichen.x@outlook.com>
2021-05-16 17:45:30 +0800

    Update init.el

--- init.el
+++ init.el
@@ -254,9 +254,9 @@ It should only modify the values of Spacemacs settings."
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(
+                         modus-vivendi
                          nord
                          modus-operandi
-                         modus-vivendi
                          fantom
                          snazzy
                          sanityinc-tomorrow-eighties
